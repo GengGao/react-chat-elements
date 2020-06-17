7f7f162e94e18d4ba54131157e5e2bec0510f946
Berni <bernhard@fittrack.io>
2020-06-17 14:43:45 +0200

    Update exported types

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,13 @@
-import MessageBox from './MessageBox/MessageBox';
-import ChatItem from './ChatItem/ChatItem';
-import SystemMessage from './SystemMessage/SystemMessage';
-import ChatList from './ChatList/ChatList';
-import MessageList from './MessageList/MessageList';
-import Input from './Input/Input';
-import Button from './Button/Button';
-import Avatar from './Avatar/Avatar';
-import Navbar from './Navbar/Navbar';
-import Dropdown from './Dropdown/Dropdown';
-import SideBar from './SideBar/SideBar';
-import Popup from './Popup/Popup';
-import ReplyMessage from './ReplyMessage/ReplyMessage';
-
-
-export {
-    MessageBox,
-    ChatItem,
-    SystemMessage,
-    ChatList,
-    MessageList,
-    Input,
-    Button,
-    Avatar,
-    Navbar,
-    Dropdown,
-    SideBar,
-    Popup,
-    ReplyMessage,
-};
+export * from './MessageBox/MessageBox';
+export * from './ChatItem/ChatItem';
+export * from './SystemMessage/SystemMessage';
+export * from './ChatList/ChatList';
+export * from './MessageList/MessageList';
+export * from './Input/Input';
+export * from './Button/Button';
+export * from './Avatar/Avatar';
+export * from './Navbar/Navbar';
+export * from './Dropdown/Dropdown';
+export * from './SideBar/SideBar';
+export * from './Popup/Popup';
+export * from './ReplyMessage/ReplyMessage';
